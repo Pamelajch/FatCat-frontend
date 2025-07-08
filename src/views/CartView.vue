c6ee6d7ad5ed51c41940a679f5ff7ac3f27efe2e
zihwei <zihwei1215@gmail.com>
2025-07-08 10:52:30 +0800

    購物車頁面

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,22 +3,54 @@
 </script>
 
 <template>
-    <div>
+    <div class=" container mb-2">
         <h2>購物車確認</h2>
         <div class="accordion col-lg-10 container mb-5" id="accordionPanelsStayOpenExample" >
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-        Accordion Item #1
+        購物車
       </button>
     </h2>
     <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+        
+                 </div>
+              </div>
+            </div>
+         </div>
     </div>
-  </div>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class=" col-12 col-md-6">
+                <div class="row mb-3 g-0">
+                    <div class="col-12 col-lg-6">
+                      
+                    </div>
+                    <!-- display:flex = d-flex/ justify-content-center 水平置中/ align-items-center 垂直置中 -->
+                    <div class="col-12" style="border: 1px solid saddlebrown;">
+                        <div >
+                            <p>優惠&促銷</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="row mb-3 g-0">
+                    <div class="col-12 col-lg-6">
+                       
+                    </div>
+                    <!-- display:flex = d-flex/ justify-content-center 水平置中/ align-items-center 垂直置中 -->
+                    <div class="col-12" style="border: 1px solid saddlebrown;">
+                        <div>
+                            <p>猜你喜歡</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
