1dd418217942087767932728629d780cbb2e878c
zihwei <zihwei1215@gmail.com>
2025-07-08 11:27:27 +0800

    購物車頁面架構

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -9,7 +9,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-        購物車
+        <h3>購物車</h3>
       </button>
     </h2>
     <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
@@ -20,6 +20,9 @@
             </div>
          </div>
     </div>
+
+    
+
     <div class="container">
         <div class="row">
             <div class=" col-12 col-md-6">
@@ -27,28 +30,37 @@
                     <div class="col-12 col-lg-6">
                       
                     </div>
-                    <!-- display:flex = d-flex/ justify-content-center 水平置中/ align-items-center 垂直置中 -->
-                    <div class="col-12" style="border: 1px solid saddlebrown;">
-                        <div >
-                            <p>優惠&促銷</p>
-                        </div>
-
-                    </div>
+                    <ul class="list-group">
+                    <li class="list-group-item">
+                        <h3>優惠&促銷</h3>
+                    </li>
+                    <li class="list-group-item">已享有優惠&可用優惠券</li>
+                    <li class="list-group-item">優惠促銷</li>
+                </ul>
+                    
                 </div>
             </div>
+            
             <div class="col-12 col-md-6">
                 <div class="row mb-3 g-0">
                     <div class="col-12 col-lg-6">
                        
                     </div>
-                    <!-- display:flex = d-flex/ justify-content-center 水平置中/ align-items-center 垂直置中 -->
-                    <div class="col-12" style="border: 1px solid saddlebrown;">
-                        <div>
-                            <p>猜你喜歡</p>
+                    <ul class="list-group">
+                    <li class="list-group-item">
+                        <h3>猜你喜歡</h3>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <button type="button" class="btn btn-danger">Left</button>
+                            <button type="button" class="btn btn-warning">Middle</button>
+                            <button type="button" class="btn btn-success">Right</button>
                         </div>
-
-                    </div>
-                </div>
+                    </li>
+                  
+                </ul>
+                    
+                </div><button type="button" class="btn btn-primary float-end">前往結帳</button>
             </div>
         </div>
     </div>
