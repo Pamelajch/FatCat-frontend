879877c67096ee4dfb6436beac47df7ac3d4786e
Rako <nicolecat28@gmail.com>
2025-07-08 11:05:19 +0800

    增加商品按鈕及安裝font awesome

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import HeaderComponent from './components/HeaderComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
 </script>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -14,6 +14,7 @@
       <!-- 右側按鈕群組 -->
       <div class="d-flex align-items-center gap-3 gap-lg-4">
         <RouterLink :to="{name:'home'}" class="icon-btn" title="首頁"><i class="bi bi-house-door"></i></RouterLink>
+        <RouterLink :to="{name:'productlist'}" class="icon-btn" title="商品"><i class="fa-solid fa-bowl-food"></i></RouterLink>
         <RouterLink :to="{name:'cart'}" class="icon-btn" title="購物車"><i class="bi bi-cart"></i></RouterLink>
         <RouterLink :to="{name:'favorite'}" class="icon-btn" title="我的最愛"><i class="bi bi-heart-fill"></i></RouterLink>
         <RouterLink :to="{name:'user'}" class="icon-btn" title="會員中心"><i class="bi bi-person-circle"></i></RouterLink>
