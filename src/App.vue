d23b42c25834cc5d655c182a98e3d7c401f17257
Rako <nicolecat28@gmail.com>
2025-07-10 08:54:32 +0800

    增加載入動畫

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,59 @@
 <script setup>
-import { RouterLink, RouterView,useRoute} from 'vue-router'
-import { computed,onMounted } from 'vue'
-import { useAuthStore } from './stores/auth'
+import { ref, onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+
 import HeaderComponent from './components/HeaderComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
 
-//獲取目前路由
-const route = useRoute()
-//計算是否需要隱藏 Header 和 Footer
-const shouldhideHeaderFooter = computed(()=>{
-  return route.meta.hideHeaderFooter || false
-})
+// 狀態：是否正在載入
+const isLoading = ref(true)
 
-//記住user登入資訊
-// 獲取auth store
-   const authStore = useAuthStore()
-      // 在組件掛載時初始化auth狀態
-      onMounted(() => {
-        authStore.initializeAuth()
-   })
+onMounted(() => {
+  // 模擬loading 過程，可以替換成實際 API 完成後再關掉
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1500)
+})
 </script>
 
 <template>
   <div>
-    <HeaderComponent v-if="!shouldhideHeaderFooter"></HeaderComponent>
-    <main>
-      <RouterView></RouterView>
-    </main>
-    <FooterComponent v-if="!shouldhideHeaderFooter"></FooterComponent>
-  </div>
+    <!--Loading 畫面 -->
+    <div v-if="isLoading" class="loading-overlay">
+      <img src="/loading.gif" alt="Loading" class="loading-img" />
+    </div>
 
+    <!--內容畫面 -->
+    <div v-else>
+      <HeaderComponent />
+      <main>
+        <RouterView />
+      </main>
+      <FooterComponent />
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #FF7F61; /* 或改成 #f6f0fa 淡紫色 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
 
+.loading-img {
+  width: 700px; /* 你可以調整大小 */
+}
 </style>
