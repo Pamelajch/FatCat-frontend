afc146191a280a5e06254f82b8bcbb5045c4bab7
Jaxie <erachael3721@gmail.com>
2025-07-14 16:09:04 +0800

    修好怪異的登入跳轉問題

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Facebook SDK 初始化
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1307390251394742',
+        cookie: true,
+        xfbml: true,
+        version: 'v18.0'
+    });
+};
+
+
 const app = createApp(App)
 const pinia = createPinia()
 
