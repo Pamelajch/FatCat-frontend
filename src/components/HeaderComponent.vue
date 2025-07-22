da5db2c58cfac55405d74d48378ff17cffd706fb
Jaxie <erachael3721@gmail.com>
2025-07-23 01:14:43 +0800

    調整搜尋框樣式

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -207,13 +207,7 @@
       <!-- 搜尋區塊 -->
       <div class="search-bar position-relative me-3" ref="searchBarRef">
         <div class="group">
-           <svg viewBox="0 0 24 24" aria-hidden="true" class="search-icon">
-              <g>
-                <path
-                  d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
-                ></path>
-             </g>
-           </svg> 
+           <i class="fa-solid fa-magnifying-glass search-icon"></i>
           <input
               v-model="searchKeyword"
               @keyup.enter="doSearch"
@@ -381,7 +375,7 @@
 /* 
 搜尋功能樣式 */
 .search-bar {
-  min-width: 220px;
+  min-width: 500px;
 }
 
 .search-dropdown {
