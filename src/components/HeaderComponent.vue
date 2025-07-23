853ff5914b8676c1cdf4b9189efcb3b2d07b1d0a
zihwei <zihwei1215@gmail.com>
2025-07-23 15:04:39 +0800

    下拉選單增加我的訂單

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -150,6 +150,7 @@
             <template v-if="isAuthenticated">
               <li><RouterLink :to="{name:'user'}" class="dropdown-item"><i class="bi bi-person-gear me-2"></i>會員中心</RouterLink></li>
               <li><RouterLink :to="{name:'feedback'}" class="dropdown-item"><i class="bi bi-envelope-arrow-up me-2"></i>進度追蹤</RouterLink></li>
+              <li><RouterLink :to="{name:'myorders'}" class="dropdown-item"><i class="fa-solid fa-scroll me-2"></i>我的訂單</RouterLink></li>
               <li><hr class="dropdown-divider"></li>
               
               <li><button @click="handleLogout" class="dropdown-item" :disabled="authStore.isLoading"><i class="bi bi-box-arrow-right me-2"></i>{{ authStore.isLoading ? '登出中...' : '登出' }}</button></li>
