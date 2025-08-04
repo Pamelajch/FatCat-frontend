ce4cc9559b937a634fc83f0b8f592aa41c923d31
Jaxie <erachael3721@gmail.com>
2025-08-04 10:15:40 +0800

    e04

--- src/components/AddressForm.vue
+++ src/components/AddressForm.vue
@@ -11,7 +11,7 @@ const props = defineProps({
     address: { type: Object, default: null },
     isSubmitting: { type: Boolean, default: false }
 })
-//////做到這裡
+
 
 
 const emit = defineEmits(['submit', 'cancel'])
@@ -47,6 +47,8 @@ const isEdit = computed(() => props.address !== null)
 
 // 計算屬性：判斷是否為超商地址 --------------------------------------------------------------------------------------------
 const isStoreAddress = computed(() => form.addressType === '1')
+//////做到這裡
+// 計算屬性：判斷是否為一般宅配地址
 // 計算屬性：判斷是否為一般宅配地址
 const isHomeAddress = computed(() => form.addressType === '0')
 
