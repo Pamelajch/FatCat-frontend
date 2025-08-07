3a5c734acfadaf0fa9674708116b76c57e86e8a4
Jaxie <erachael3721@gmail.com>
2025-08-07 14:12:42 +0800

    合併jj11

--- src/components/AddressManager.vue
+++ src/components/AddressManager.vue
@@ -270,7 +270,7 @@ const setDefaultAddress = async (addressId) => {
                     />
                 </div>
             </div>
-        </div>
+        </div> 
     </div>
 </template>
 <style scoped>
